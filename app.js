51f6f887442f5dbc1db0fa6a1b04f742e1011620
KOHA <kotohana5706@edcan.kr>
2016-05-27 12:08:02 +0000

    new api request : /auth/getSelfinfo, /auth/changeSelfProfile

--- app.js
+++ app.js
@@ -2,14 +2,38 @@ var port = 8000;
 
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
+// routes
 var routes = require('./routes');
 var index = require('./routes/index');
-var multer  = require('multer');
+var admin = require('./routes/admin');
+var auth = require('./routes/auth');
+var notice = require('./routes/notice');
+var attendence = require('./routes/attendance');
 var app = express();
 var http = require('http').Server(app);
 
-app.use('/', index);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(session({
+//     secret: '314159265358979323846264338',
+//     resave: false,
+//     saveUninitialized: true
+// }));
+
+app.use('/admin', admin);
+app.use('/auth', auth);
+app.use('/notice', notice);
+app.use('/attendence', attendence);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(function(req, res, next) {
   var err = new Error('404 Not Found');
   err.status = 404;
